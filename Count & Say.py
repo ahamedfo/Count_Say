febb6ef509b7a82b8c8d4cd30ac0061507aaa5b1
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-12 15:57:16 -0400

    Completed count & say O(n^2)

--- Count & Say.py
+++ Count & Say.py
@@ -1,11 +1,23 @@
 class Solution(object):
     def countAndSay(self, n):
         num_map = {}
-        for i in range(0, n):
+        strings = ""
+        for i in range(1, n):
+            checker = {}
             if i == 1:
                 num_map = 1
             else:
-                num_map[i] = num_map[i - 1]
+                temp_num = float("inf")
+                for number in num_map[i]:
+                    if number not in checker:
+                        checker[number] = 0
+                    elif number in checker:
+                        checker[number] += 1
+                    if checker[number] > 0 and temp_num != number:
+                        strings += str(number) * checker[number]
+                    temp_num = number
+
+
         """
         :type n: int
         :rtype: str
@@ -15,3 +27,10 @@ class Solution(object):
 2 -> 11
 3 -> 21
 4 -> 1211
+5 -> 111221
+6 -> 312211
+
+1 = 1
+2 = 1 + 10
+3 = 10 + 11
+4 =
