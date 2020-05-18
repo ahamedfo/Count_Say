8cdd3a7c9c26ca47c3f40ac24c94edf38cdf7887
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-18 14:20:06 -0400

    Completed count & say O(n^2) now

--- Count & Say.py
+++ Count & Say.py
@@ -1,40 +1,23 @@
 class Solution(object):
     def countAndSay(self, n):
         num_map = {}
-        strings = ""
-        for i in range(1, n):
-            checker = {}
-            if i == 1:
-                num_map = 1
-            else:
-                temp_num = float("inf")
-                for number in num_map[i-1]:
-                    if number not in checker:
-                        checker[number] = 1
-                    elif number in checker:
-                        checker[number] += 1
-                    if temp_num != number and temp_num != float("inf"):
-                        strings += str(temp_num) * checker[temp_num]
-                    temp_num = number
-                num_map[i] = int(strings)
-        return str
+        if n == 1:
+            return "1"
+        if n == 2:
+            return "11"
 
-
-        """
-        :type n: int
-        :rtype: str
-        """
-
-
-
-1 -> 1
-2 -> 11
-3 -> 21
-4 -> 1211
-5 -> 111221
-6 -> 312211
-
-1 = 1
-temp_number = 0
-checker -> 1 = 0
-temp_number = 1
+        strings = "11"
+        for i in range(3, n + 1):
+            frequency = 1
+            build_me = ""
+            k = 0
+            for i, number in enumerate(strings):
+                if i > 0 and number == strings[i - 1]:
+                    frequency += 1
+                elif i > 0 and number != strings[i - 1]:
+                    build_me += (str(frequency) + str(strings[i - 1]))
+                    frequency = 1
+                if i == len(strings) - 1:
+                    build_me += (str(frequency) + str(strings[i]))
+            strings = build_me
+        return strings
